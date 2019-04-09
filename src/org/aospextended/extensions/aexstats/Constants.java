b4397b59976a5ad62fb84105c9ffb8abef38b560
Anis Noorudheen <35500897+anisnoorudheen@users.noreply.github.com>
2019-04-09 18:32:31 +0530

    Update Constants.java

--- src/org/aospextended/extensions/aexstats/Constants.java
+++ src/org/aospextended/extensions/aexstats/Constants.java
@@ -1,12 +1,12 @@
 package org.aospextended.extensions.aexstats;
 
 /**
- * Created by ishubhamsingh on 25/9/17.
+ * Created by Anisn on 7/4/19.
  */
 
 public class Constants {
-    public static final String BASE_URL= "https://stats.aospextended.com/";
-    public static final String TAG= "AEXstats";
+    public static final String BASE_URL= "https://GitHub.com/aosep.com/";
+    public static final String TAG= "AOSEPstats";
     public static final String PUSH_OPERATION = "push";
 
     public static final String SUCCESS = "success";
@@ -15,9 +15,9 @@ public class Constants {
     public static final String LAST_BUILD_DATE = "lastBuild";
     public static final String LAST_CUSTOM_FINGERPRINT = "lastBuildFingerprint";
 
-    public static final String KEY_DEVICE = "ro.aex.device";
-    public static final String KEY_MODEL = "ro.product.model";
-    public static final String KEY_VERSION = "6.4";
-    public static final String KEY_BUILD_TYPE = "ro.extended.releasetype";
-    public static final String KEY_BUILD_DATE = "ro.build.date";
+    public static final String KEY_DEVICE = "Don't know.. then Google";
+    public static final String KEY_MODEL = "check Phone box";
+    public static final String KEY_VERSION = "2.0";
+    public static final String KEY_BUILD_TYPE = "Whatever it is u r screwed";
+    public static final String KEY_BUILD_DATE = "try remembering self lol";
 }
